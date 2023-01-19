244cbbafcff40aa50de28ee15eff2e2a718db0c3
Patrik Nyblad <patrik.nyblad@easypark.net>
2023-01-19 15:17:35 +0100

    Add missing fs require statement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const glob = require('@actions/glob');
+const fs = require('fs');
 
 const STATUS = {
   QUEUED: 'queued',
