7173c1b0ac8d04a289dfc63383c8c1dce0aa8f41
Patrik Nyblad <patrik.nyblad@easypark.net>
2023-01-19 15:17:35 +0100

    Add missing fs require statement

--- dist/index.js
+++ dist/index.js
@@ -12215,6 +12215,7 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const glob = __nccwpck_require__(8090);
+const fs = __nccwpck_require__(7147);
 
 const STATUS = {
   QUEUED: 'queued',
